5ae8df76e6b4a5e2fd00d7fc6ad020b7d1922929
i0rdan <kazusev2000@mail.ru>
2021-11-30 19:45:02 +0300

    about us + material

---
+++ src/app/about-us/about-us.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-about-us',
+  templateUrl: './about-us.component.html',
+  styleUrls: ['./about-us.component.css']
+})
+export class AboutUsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  count: number = 2;
+
+  foods: any = [
+    {value: 'steak-0', viewValue: 'Steak'},
+    {value: 'pizza-1', viewValue: 'Pizza'},
+    {value: 'tacos-2', viewValue: 'Tacos'},
+  ];
+
+}
